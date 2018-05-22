80bb662f4c21b151db2ec75a768306e58cd85434
Alexandre Blondeau <deformhead@gmail.com>
2018-05-22 13:13:19 +0200

    update export type

--- sources/distance.js
+++ sources/distance.js
@@ -1,26 +1,33 @@
-export function diagonal(a, b) {
+function diagonal(a, b) {
 
     const [xa, ya] = a;
     const [xb, yb] = b;
 
     // return the diagonal distance between a and b
     return Math.max(Math.abs(xa - xb), Math.abs(ya - yb));
-};
+}
 
-export function euclidean(a, b) {
+function euclidean(a, b) {
 
     const [xa, ya] = a;
     const [xb, yb] = b;
 
     // return the euclidean distance between a and b
     return Math.sqrt(Math.pow(xa - xb, 2) + Math.pow(ya - yb, 2));
-};
+}
 
-export function manhattan(a, b) {
+function manhattan(a, b) {
 
     const [xa, ya] = a;
     const [xb, yb] = b;
 
     // return the manhattan distance between a and b
     return Math.abs(xa - xb) + Math.abs(ya - yb);
+}
+
+export {
+
+    diagonal,
+    euclidean,
+    manhattan
 };
